eb0d20275e521e945ea10b0e930824384233d811
Maaz Hasan <36072953+mhasan95@users.noreply.github.com>
2021-02-16 13:15:08 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import NavMenu from './components/NavMenu';
-import FirstContainer from './components/FirstContainer';
 import UserComponent from './components/HomeContainer';
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,8 +11,14 @@ import 'devextreme/dist/css/dx.light.css';
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ backgroundColor:'#ebf0f4',height:'98vh'}}>
+      <div className="App" style={{ backgroundColor:'#ebf0f4',height:'100vh'}}>
         <NavMenu />
+        <footer class="footer" style={{backgroundColor:'#0055a5',height:'50px'}}>
+      <div class="container">
+        <span class="text-muted">Copyright: 2021
+        </span>
+      </div>
+    </footer>
       
       </div>
     );
